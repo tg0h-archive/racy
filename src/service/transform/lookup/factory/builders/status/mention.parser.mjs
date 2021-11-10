9974b4362f904ce6868e2a9f429f8fcc0137456a
tim goh <timothysl_goh@certisgroup.com>
2021-11-10 10:44:31 +0800

    fix(status): parse new mention format from gitlab (1h)
    see ARG-2937 and ARG-3124 for some examples of the new gitlab mention
    format.
    
    note the multiple url links in the commit mention where before there
    would only be 1 link for the user name and 1 link for the commit ref or
    merge request number.

--- src/service/transform/lookup/factory/builders/status/mention.parser.mjs
+++ src/service/transform/lookup/factory/builders/status/mention.parser.mjs
@@ -13,10 +13,13 @@ function parseMention(mention) {
 function parseBody(body) {
     // the mention type has a space, eg 'merge request'
     let bodyRegex = /\[(?<userName>.*?)\|(?<userUrl>.*?)].*\[a (?<mentionType>.*?) of (?<projectNameFull>.*?)\|(?<mentionUrl>.*?)](:\n')(?<mentionDescription>.*)'/
-    let bodyMatches = body.match(bodyRegex)
+    // see ARG-2937 for an example of a strange commit mention message that requires an alternative regex
+    let bodyRegex2 = /\[(?<userName>.*?)\|(?<userUrl>.*?)].*\[a (?<mentionType>.*?)\|(?<mentionUrl>.*?)](?<mentionDescription>.*)/
 
-    // sample commit mentionUrl        https://git.ads.certis.site/optimax/optimax-apps/optimax-cc/-/commit/86e4a2d5b3473de655981d9fe25435e2a8dd4ed2
-    // sample merge request mentionUrl https://git.ads.certis.site/optimax/optimax-apps/optimax-cc/-/merge_requests/896
+    let bodyMatches = body.match(bodyRegex) ?? body.match(bodyRegex2)
+
+    // sample commit mentionUrl        https://git.blah/optimax/optimax-apps/optimax-cc/-/commit/86e4a2d5b3473de655981d9fe25435e2a8dd4ed2
+    // sample merge request mentionUrl https://git.blah/optimax/optimax-apps/optimax-cc/-/merge_requests/896
     let mentionUrl = bodyMatches?.groups.mentionUrl
 
     // we must escape / by adding a \ to get \/
